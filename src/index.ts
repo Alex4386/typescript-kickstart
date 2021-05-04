dcf2ee41b48e84cddd7a86c07399d3a79b5bc152
alex4386 <psh010209@gmail.com>
2021-05-05 02:06:34 +0900

    chore: remove sample log code

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,3 @@ const isVerbose = process.argv.map((n) => n.toLowerCase()).includes('--verbose')
 export const logger = buildLogger({
   level: isVerbose ? 'silly' : undefined,
 });
-
-// example log data.
-logger.silly('This is a silly message');
-logger.verbose('Not Implemented.');
-logger.debug('This is a debug message');
-logger.info('This is a INFO');
-logger.warn('This is a WARN');
-logger.error('Not Implemented.', { assd: 'bummer', ang: 'gimotti' });
